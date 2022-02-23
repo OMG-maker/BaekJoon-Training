089e4f5cd13ad043140518b5cd98519b599ecda4
mgOh <issac_oh@naver.com>
2022-02-23 15:01:22 +0900

    add: 11279 - 최대 힙 (우선순위 큐)

--- 11279.py
+++ 11279.py
@@ -8,8 +8,8 @@ for i in range(N):
     term = int(sys.stdin.readline())
     if term == 0:
         if len(pq) != 0:
-            print(heapq.heappop(pq))
+            print(-1 * heapq.heappop(pq))
         else:
             print(0)
     else:
-        heapq.heappush(pq, term)
+        heapq.heappush(pq, -term)
